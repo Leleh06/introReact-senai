64d171f23a1e12294c774f50540d05ea24e3c943
Leleh06 <leticyasilvad1608@gmail.com>
2025-05-26 22:18:37 -0300

    feat:Aula sobre React

---
+++ ola/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+  return (
+    <div className='Body'>
+       <h2 className='titulo2'>Dinos são legais</h2>
+       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ipsum, aliquid expedita nulla atque nisi. Maiores omnis sed, eaque, repellat et mollitia optio sunt quaerat animi, iusto totam minus est.</p>
+    </div>
+  )
+}
+
+export default Body
