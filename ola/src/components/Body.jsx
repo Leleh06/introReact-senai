fc939871b84575c0cdad9f9bc4529d131f778f63
Leleh06 <leticyasilvad1608@gmail.com>
2025-05-28 21:55:36 -0300

    feat: React site Harry Potter, colocar imagem uma do lado do outro

--- ola/src/components/Body.jsx
+++ ola/src/components/Body.jsx
@@ -1,69 +1,31 @@
 import { useState } from 'react'
 import './Body.css'
 
-function Body() {
-  const [cont, setCont] = useState(0)
-  const[inputValor,setInputValor] = useState("")
-  const[inputN1, setInputN1] = useState('')
-  const[inputN2, setInputN2] = useState('')
-  function aumentar() {
-    setCont(cont + 1)
-  }
-
-  function diminuir(){
-    if (cont > 0) {
-      setCont(cont - 1)
-    }
-  }
-
-    function alterar(){
-      setCont(Number(inputValor))
-    }
-
-    function somar(){
-      const somar = Number(inputN1) + Number (inputN2)
-      setCont((somar))
-    }
-
-    function dividir(){
-      setCont(Number(inputN1) - Number(inputN2))
-    }
-
-    function multiplicacao(){
-      setCont(Number(inputN1) * Number(inputN2))
-    }
-
+function Body(props) {
+  console.log(props.characters);
   return (
     <div className='Body'>
-      <h2 className='titulo2'>Dinos são legais</h2>
-      <div className='cont'>
-      <button onClick={diminuir}>-</button>
-      <p>Contagem: {cont}</p>
-      <button onClick={aumentar}>+</button>
-      </div>
+      <div className='grid'>
+        {
+          props.characters.map((character) => (
+            <div className='card'>
+              {character.image ? (
+                <img src={character.image} alt={character.name} />
+              ) : (
+                <div className='noImage'>Imagem indisponivel</div>
+              )
 
-      <div className='form'>
-        <input type="number"
-         value={inputValor}
-         onChange={(e) => setInputValor(e.target.value)}
-        />
-       <button onClick={alterar}>Alterar</button>
-      </div>
+              }
 
-      <div className='calculadera'>
-        <input type="number" 
-        value={inputN1}
-        onChange={(e) => setInputN1(e.target.value)}
-        />
-        <button onClick={somar}>+</button>
-        <button onClick={dividir}>-</button>
-        <button onClick={multiplicacao}>*</button>
-        <input type="number" 
-         value={inputN2}
-         onChange={(e) => setInputN2(e.target.value)}
-        />
+              <h2>{character.name}</h2>
+              <p>
+                <strong>Casa:</strong>
+                {character.house || "Desconhecida"}
+              </p>
+            </div>
+          ))
+        }
       </div>
-
     </div>
   )
 }
